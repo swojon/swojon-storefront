2e98560a584a4fe91e91137a0c4145665d05bb06
MousumiMitu <mousumitu@gmail.com>
2023-07-30 23:58:23 +0600

    format editing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+"use client";
 
 import type { NextPage } from "next";
 
@@ -7,54 +6,18 @@ import { getClient } from "@/lib/client";
 
 import { gql } from "@apollo/client";
 
-const query = gql`query Now {
+const query = gql`
+  query Now {
     now(id: "1")
-}`;
-
+  }
+`;
 
 const Home: NextPage = () => {
-  // const { data } = await getClient().query({ query });
-  // const authState = useSelector(selectAuthState);
-  // const dispatch = useDispatch();
-  // return <main>{data.now}</main>;
   return (
     <div>
-      {/* <div>{authState ? "Logged in" : "Not Logged In"}</div>
-      <button
-        onClick={() =>
-          authState
-            ? dispatch(setAuthState(false))
-            : dispatch(setAuthState(true))
-        }
-      >
-        {authState ? "Logout" : "LogIn"}
-      </button> */}
-
-
+      <h1>hello</h1>
     </div>
   );
 };
 
-// export async function getStaticProps() {
-//   const { data } = await getClient().query({
-//     query: gql`
-//       query NextLaunch {
-//         launchNext {
-//           mission_name
-//           launch_date_local
-//           launch_site {
-//             site_name_long
-//           }
-//         }
-//       }
-//     `,
-//   });
-//   return {
-//     props: {
-//       nextLaunch: data.launchNext,
-//     },
-//  };
-// }
-
 export default Home;
-
