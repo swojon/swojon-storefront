b2760e789af32c6e7bb083334e4ac208c42a3142
MousumiMitu <mousumitu@gmail.com>
2023-08-15 00:36:46 +0600

    megamenu added

---
+++ components/MegaMenu/MegaMenu.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MegaMenu = () => {
+  return (
+    <div className="bg-white w-full">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="">
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+          <h2 className="text-2xl py-1 mb-1 font-semibold">Electronics</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MegaMenu;
