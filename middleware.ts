9078041f1158c65295ac6b66321259753d47ff2e
Iqbal Mohammad Rhidwan <iqbalrhidwan52@gmail.com>
2023-10-24 16:49:02 +0600

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,42 @@
-export {default} from 'next-auth/middleware';
+// export {default} from 'next-auth/middleware';
 
-export const config = {
-    matcher: [
-        "/user/:path*",
-        // "/messages/:path*"
-    ]
+import { setCookie } from "cookies-next";
+import { NextRequest, NextResponse } from "next/server"
+
+export const protectedRoutes = ["/chat"]
+
+export const middleware = (request: NextRequest) => {
+    // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
+    // Getting cookies from the request using the `RequestCookies` API
+    
+    const authToken = request.nextUrl.searchParams.get('token');
+    if (authToken){
+        console.log("token is set. ")
+        setCookie("authorization", authToken, {secure:true })
+        let query = ""
+        request.nextUrl.searchParams.delete('token')
+        const res = NextResponse.redirect(request.nextUrl)
+        res.cookies.set("aurhoriza", authToken)
+        return res
+    }
+    console.log("pathName", request.nextUrl.pathname)
+
+    protectedRoutes.forEach((route) => {
+        if (!!request.nextUrl.pathname.match(route)){
+            console.log("Accessing a protected route")
+            let cookie = request.cookies.get('authorization')
+            console.log("cookie", cookie)
+            console.log("cookie", request.cookies)
+            if (!cookie){
+                // console.log(request.nextUrl.pathname)
+                const url = request.nextUrl.clone()
+                url.pathname = `/signin`;
+                const res = NextResponse.redirect(url);
+                res.cookies.set("redirectTo", request.nextUrl.pathname, {maxAge: 10 * 60})
+                return res
+            }
+        }
+    })
+    
+    
 }
