01f388ad5e9eed7e078413704f11b8bdc7c2db78
MousumiMitu <mousumitu@gmail.com>
2023-11-13 00:32:59 +0600

    max wdth

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -1,84 +1,115 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import heartIcon  from "@/public/assets/heartIcon.png";
-import heartIconFilled from "@/public/assets/heartIconFilled.svg.svg"
+import heartIcon from "@/public/assets/heartIcon.png";
+import heartIconFilled from "@/public/assets/heartIconFilled.svg.svg";
 import time from "@/public/assets/time.png";
 import user from "@/public/user1.jpg";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { setModalOpen } from "@/app/redux/modalSlice";
 import { timeAgo } from "@/lib/helpers/timeAgo";
-import { ListListingsDocument, useAddFavoriteMutation, useRemoveFavoriteMutation } from "@/apollograph/generated";
+import {
+  ListListingsDocument,
+  useAddFavoriteMutation,
+  useRemoveFavoriteMutation,
+} from "@/apollograph/generated";
 
 const ProductCard = ({ card: listing }: { card: any }) => {
   const dispatch = useDispatch();
-  const authState = useSelector((state: any ) => state.auth)
-  const [addFavorite, {data:addData, loading:addLoading, error:addErrror}] = useAddFavoriteMutation()
-  const [removeFavorite, {data:removeData, loading:removeLoading, error:removeError}] = useRemoveFavoriteMutation()
-  const handleFavoriteAdd = (listingId:number, userId: any) => {
+  const authState = useSelector((state: any) => state.auth);
+  const [
+    addFavorite,
+    { data: addData, loading: addLoading, error: addErrror },
+  ] = useAddFavoriteMutation();
+  const [
+    removeFavorite,
+    { data: removeData, loading: removeLoading, error: removeError },
+  ] = useRemoveFavoriteMutation();
+  const handleFavoriteAdd = (listingId: number, userId: any) => {
     addFavorite({
       variables: {
-        listingId, 
-        userId
+        listingId,
+        userId,
       },
-      update(cache, {data}) {
+      update(cache, { data }) {
         // console.log("updating cache", cache,  data)
-        const cId = cache.identify(listing)
+        const cId = cache.identify(listing);
         // console.log("cache id", cId)
         cache.modify({
-          id: cId, 
+          id: cId,
           fields: {
-            favoriteCount(prev) {return prev + 1},
-            favoriteStatus(prev){return true}
-          }
-        })
+            favoriteCount(prev) {
+              return prev + 1;
+            },
+            favoriteStatus(prev) {
+              return true;
+            },
+          },
+        });
         // console.log("cache updated", cache)
-      }
-    })
-  }
-  const handleFavoriteRemove = (listingId:number, userId:any) => {
+      },
+    });
+  };
+  const handleFavoriteRemove = (listingId: number, userId: any) => {
     removeFavorite({
       variables: {
         listingId,
-        userId
+        userId,
       },
-      update(cache, {data}) {
+      update(cache, { data }) {
         // console.log("updating cache", cache,  data)
-        const cId = cache.identify(listing)
+        const cId = cache.identify(listing);
         // console.log("cache id", cId)
         cache.modify({
-          id: cId, 
+          id: cId,
           fields: {
-            favoriteCount(prev) {return prev - 1},
-            favoriteStatus(prev){return false}
-          }
-        })
+            favoriteCount(prev) {
+              return prev - 1;
+            },
+            favoriteStatus(prev) {
+              return false;
+            },
+          },
+        });
         // console.log("cache updated", cache)
-      }
-      
-    })
-  }
+      },
+    });
+  };
   return (
     <div className="  rounded-md bg-whiteColor border border-[#EFEFEF] p-2.5 hover:shadow-lg  cursor-pointer transition ease-in-out delay-150 duration-300">
-      <div className="lg:h-[210px] md:h-[170px] h-[130px] relative overflow-hidden rounded-tl-md  rounded-tr-md">
+      <div
+        className="xl:h-[
+215px] lg:h-[210px] md:h-[170px] h-[130px] relative overflow-hidden rounded-tl-md  rounded-tr-md"
+      >
         <Link href={`/products/${listing.id}`}>
           <Image
-            src={listing.media.length > 0 ? listing.media[0].url : "/assets/pro1.png"}
+            src={
+              listing.media.length > 0
+                ? listing.media[0].url
+                : "/assets/pro1.png"
+            }
             width={500}
             height={500}
             alt="product banner"
             className="h-full w-full object-cover rounded-tl-md  rounded-tr-md hover:scale-110 transition ease-in-out delay-150 duration-300 "
           />
         </Link>
-        {listing.favoriteStatus ? 
-        <div onClick={() => handleFavoriteRemove(listing.id, authState.user.id)} className="absolute right-0 top-0 m-3 w-7 h-7 flex justify-center items-center border border-[#EFEFEF] rounded-full bg-whiteColor hover:scale-105 transition ease-in-out delay-150 duration-300">
-          <Image src={heartIconFilled} alt="heart icon filled" />
-        </div> : 
-        <div onClick={() => handleFavoriteAdd(listing.id, authState.user.id)} className="absolute right-0 top-0 m-3 w-7 h-7 flex justify-center items-center border border-[#EFEFEF] rounded-full bg-whiteColor hover:scale-105 transition ease-in-out delay-150 duration-300">
-          <Image src={heartIcon} alt="heart icon" />
-        </div>
-        }
+        {listing.favoriteStatus ? (
+          <div
+            onClick={() => handleFavoriteRemove(listing.id, authState.user.id)}
+            className="absolute right-0 top-0 m-3 w-7 h-7 flex justify-center items-center border border-[#EFEFEF] rounded-full bg-whiteColor hover:scale-105 transition ease-in-out delay-150 duration-300"
+          >
+            <Image src={heartIconFilled} alt="heart icon filled" />
+          </div>
+        ) : (
+          <div
+            onClick={() => handleFavoriteAdd(listing.id, authState.user.id)}
+            className="absolute right-0 top-0 m-3 w-7 h-7 flex justify-center items-center border border-[#EFEFEF] rounded-full bg-whiteColor hover:scale-105 transition ease-in-out delay-150 duration-300"
+          >
+            <Image src={heartIcon} alt="heart icon" />
+          </div>
+        )}
       </div>
 
       <div className="md:pt-3 pt-1 flex flex-row  justify-between items-center font-lexed ">
@@ -87,12 +118,16 @@ const ProductCard = ({ card: listing }: { card: any }) => {
             {listing.title}
           </h6>
         </Link>
-        <span className="text-activeColor md:text-base text-sm">TK, {listing.price}</span>
+        <span className="text-activeColor md:text-base text-sm">
+          TK, {listing.price}
+        </span>
       </div>
 
       <div className="flex items-center  text-secondColor">
         <Image src={time} alt="time icon" />
-        <span className="text-xs font-lexed ps-1">{timeAgo(listing.dateCreated)}</span>
+        <span className="text-xs font-lexed ps-1">
+          {timeAgo(listing.dateCreated)}
+        </span>
       </div>
 
       <div className="flex items-center md:space-x-2 space-x-1 md:py-4 py-2">
