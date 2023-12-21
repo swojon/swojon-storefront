90d557d57cea32a4753a6fe2df6dd2e5a4c14225
MousumiMitu <mousumitu@gmail.com>
2023-12-21 17:59:55 +0600

    preview grid layout

--- components/Uploads/UploadImage.tsx
+++ components/Uploads/UploadImage.tsx
@@ -25,98 +25,219 @@ const UploadImage = () => {
 
       <div className="h-[441px] w-full">
         {imageCount.length > 0 ? (
-          <div className="flex items-center gap-5 w-full h-full">
-            {/* <div className="w-[65%] h-full rounded-2xl border relative">
-              <Image
-                src={imageCount[0]?.url}
-                width={900}
-                height={900}
-                className="w-full h-full rounded-2xl object-cover"
-                alt="product"
-              />
+          // <div className="flex items-center gap-5 w-full h-full">
 
-              <div className="absolute right-3 top-3 flex items-center gap-3">
-                <div className="w-12 h-12 border border-gray-100 bg-white rounded-full flex items-center justify-center">
-                  <Image
-                    src="/assets/pen.png"
-                    alt="edit"
-                    width={100}
-                    height={100}
-                    className="w-5"
-                  />
-                </div>
-                <div className="w-12 h-12 border border-gray-100 bg-white rounded-full flex items-center justify-center">
-                  <Image
-                    src="/assets/delete.png"
-                    alt="edit"
-                    width={100}
-                    height={100}
-                    className="w-5"
-                  />
-                </div>
-              </div>
-            </div> */}
-            <div
-              className={`w-full h-full  grid grid-cols-5 grid-rows-2 gap-5`}
-            >
-              {imageCount.map((item: any, index: any) => (
-                <div
-                  key={index}
-                  className={`row-span-${index === 0 ? "2" : "1"} col-span-${
-                    index === 0 ? 3 : 1
-                  } rounded-2xl border relative`}
-                >
-                  <Image
-                    src={item.url}
-                    width={900}
-                    height={900}
-                    className="w-full h-full rounded-2xl object-cover"
-                    alt="product"
-                  />
-                  <div className="absolute right-2 top-2 flex items-center gap-3">
-                    <div className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center">
-                      <Image
-                        src="/assets/pen.png"
-                        alt="edit"
-                        width={100}
-                        height={100}
-                        className="w-4"
-                      />
-                    </div>
-                    <div
-                      onClick={() => deleteImage(index)}
-                      className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center"
-                    >
-                      <Image
-                        src="/assets/delete.png"
-                        alt="edit"
-                        width={100}
-                        height={100}
-                        className="w-4"
-                      />
+          //   <div
+          //     className={`w-full border  h-full  grid grid-cols-5 grid-rows-2  gap-5`}
+          //   >
+          //     {imageCount.map((item: any, index: any) => (
+          //       <div
+          //         key={index}
+          //         className={`h-${
+          //           index === 0 ? "[441px]" : "[210.5px]"
+          //         } row-span-${index === 0 ? 2 : 1} col-span-${
+          //           index === 0 && 3
+          //         } rounded-2xl border relative`}
+          //       >
+          //         <Image
+          //           src={item.url}
+          //           width={900}
+          //           height={900}
+          //           className="w-full h-full rounded-2xl object-cover"
+          //           alt="product"
+          //         />
+          //         <div className="absolute right-2 top-2 flex items-center gap-3">
+          //           <div className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center">
+          //             <Image
+          //               src="/assets/pen.png"
+          //               alt="edit"
+          //               width={100}
+          //               height={100}
+          //               className="w-4"
+          //             />
+          //           </div>
+          //           <div
+          //             onClick={() => deleteImage(index)}
+          //             className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center"
+          //           >
+          //             <Image
+          //               src="/assets/delete.png"
+          //               alt="edit"
+          //               width={100}
+          //               height={100}
+          //               className="w-4"
+          //             />
+          //           </div>
+          //         </div>
+          //       </div>
+          //     ))}
+
+          //     <div
+          //       className={` rounded-2xl border ${
+          //         imageCount.length === 1
+          //           ? "col-span-2 h-[441px]"
+          //           : imageCount.length === 2
+          //           ? "col-span-2 h-[210.5px]"
+          //           : imageCount.length === 3
+          //           ? "col-span-2 h-[210.5px]"
+          //           : imageCount.length > 4
+          //           ? "hidden"
+          //           : ""
+          //       }  `}
+          //     >
+          //       <UploadArea
+          //         imageCount={imageCount}
+          //         setImageCount={setImageCount}
+          //       />
+          //     </div>
+          //   </div>
+          // </div>
+
+          <div className="grid grid-rows-2 grid-cols-5 gap-4 w-full h-full ">
+            {imageCount.length === 1 ? (
+              <>
+                {imageCount.map((item: any, index: any) => (
+                  <div
+                    key={index}
+                    className={`
+                 col-span-3 row-span-2  rounded-2xl border relative`}
+                  >
+                    <Image
+                      src={item.url}
+                      width={900}
+                      height={900}
+                      className="w-full  h-full rounded-2xl object-cover"
+                      alt="product"
+                    />
+                    <div className="absolute right-2 top-2 flex items-center gap-3">
+                      <div className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center">
+                        <Image
+                          src="/assets/pen.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
+                      <div
+                        onClick={() => deleteImage(index)}
+                        className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center"
+                      >
+                        <Image
+                          src="/assets/delete.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </>
+            ) : imageCount.length === 2 ? (
+              <>
+                {imageCount.map((item: any, index: any) => (
+                  <div
+                    key={index}
+                    className={`
+               row-span-${index === 0 ? 2 : 1} col-span-${index === 0 ? 3 : 2}
+                rounded-2xl border relative`}
+                  >
+                    <Image
+                      src={item.url}
+                      width={900}
+                      height={900}
+                      className="w-full  h-full rounded-2xl object-cover"
+                      alt="product"
+                    />
+                    <div className="absolute right-2 top-2 flex items-center gap-3">
+                      <div className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center">
+                        <Image
+                          src="/assets/pen.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
+                      <div
+                        onClick={() => deleteImage(index)}
+                        className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center"
+                      >
+                        <Image
+                          src="/assets/delete.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <>
+                {imageCount.map((item: any, index: any) => (
+                  <div
+                    key={index}
+                    className={`
+               row-span-${index === 0 ? 2 : 1} col-span-${index === 0 ? 3 : 1}
+                  rounded-2xl border relative`}
+                  >
+                    <Image
+                      src={item.url}
+                      width={900}
+                      height={900}
+                      className="w-full  h-full rounded-2xl object-cover"
+                      alt="product"
+                    />
+                    <div className="absolute right-2 top-2 flex items-center gap-3">
+                      <div className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center">
+                        <Image
+                          src="/assets/pen.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
+                      <div
+                        onClick={() => deleteImage(index)}
+                        className="w-10 h-10 border border-gray-100 bg-white rounded-full flex items-center justify-center"
+                      >
+                        <Image
+                          src="/assets/delete.png"
+                          alt="edit"
+                          width={100}
+                          height={100}
+                          className="w-4"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            )}
 
-              <div
-                className={` rounded-2xl border ${
-                  imageCount.length === 1
-                    ? "col-span-2 h-[441px]"
-                    : imageCount.length === 2
-                    ? "col-span-2 h-[210.5px]"
-                    : imageCount.length === 3
-                    ? "col-span-2 h-[210.5px]"
-                    : imageCount.length > 4
-                    ? "hidden"
-                    : ""
-                }  `}
-              >
-                <UploadArea
-                  imageCount={imageCount}
-                  setImageCount={setImageCount}
-                />
-              </div>
+            <div
+              className={` rounded-2xl border ${
+                imageCount.length === 1
+                  ? "col-span-2 h-[441px]"
+                  : imageCount.length === 2
+                  ? "col-span-2 h-[210.5px]"
+                  : imageCount.length === 3
+                  ? "col-span-2 h-[210.5px]"
+                  : imageCount.length > 4
+                  ? "hidden"
+                  : ""
+              }  `}
+            >
+              <UploadArea
+                imageCount={imageCount}
+                setImageCount={setImageCount}
+              />
             </div>
           </div>
         ) : (
