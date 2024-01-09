1665df6bfa8f49b1358afc75d26296c4a691dfb5
Iqbal Mohammad Rhidwan <iqbalrhidwan52@gmail.com>
2024-01-09 19:21:40 +0600

    responsive initiate [partial]

--- app/(pages)/(pages)/layout.tsx
+++ app/(pages)/(pages)/layout.tsx
@@ -1,12 +1,20 @@
+'use client'
 import React, { useState } from "react";
 import SideBar from "@/components/SideBar/SideBar";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { usePathname, useSearchParams } from "next/navigation";
+import useIsMobile from "@/lib/hooks/useIsMobile";
 
 interface Iprops {
   children: React.ReactNode;
 }
 
 function layout({ children }: Iprops) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const sidebar = searchParams.get("sidebar");
+  const isMobile = useIsMobile();
+  console.log("pathname", pathname)
   return (
     <>
       <section className=" bg-white">
@@ -16,11 +24,14 @@ function layout({ children }: Iprops) {
           <span className="text-xs text-primaryColor">My Points</span>
         </div>
         <div className="flex custom-container  justify-between">
+          {(!isMobile || (pathname === "/profile" && sidebar !== "show")) &&  
           <div className="lg:w-[22%] md:w-[20%] w-[18%]">
             <SideBar />
           </div>
-
+          }
+          {((isMobile || sidebar === "hide") || pathname !== "/profile") && 
           <div className="lg:w-[75%] w-full">{children}</div>
+          }
         </div>
       </section>
     </>
