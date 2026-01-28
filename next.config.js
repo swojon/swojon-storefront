500ab06e12bbba78ccbf65ceb618d8fb89d6f9db
Iqbal Mohammad Rhidwan <iqbalrhidwan52@gmail.com>
2026-01-28 12:24:15 +0600

    optimation: off

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         serverActions: true,
     },
     images: {
-        unoptimized: false,
+        unoptimized: true,
         remotePatterns: [
             {
                 hostname: 'localhost',
