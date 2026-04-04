5a10f97c2e3c849658e24ddd1ea43fc12c4852b0
Iqbal Mohammad Rhidwan <iqbalrhidwan52@gmail.com>
2026-04-04 14:23:17 +0600

    gtag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import NotificationDrawerNew from "@/components/Notification/NotificationDrawerN
 import FacebookPixel from "@/components/FacebookPixel";
 import CartDrawer from "@/components/Cart/CartDrawer";
 import FloatingCart from "@/components/Cart/CartIcon";
+import Script from "next/dist/client/script";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -51,6 +52,20 @@ export default async function RootLayout({ children }: Iprops) {
   return (
     <html lang="en" className={nunito.variable}>
       <head>
+          
+          <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-59R8D5GT');
+            `,
+          }}
+        />
         {/* <noscript>
           <img
             height="1"
@@ -62,6 +77,15 @@ export default async function RootLayout({ children }: Iprops) {
         </noscript> */}
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-59R8D5GT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+      
         <ReduxProviders>
           <NextAuthProvider>
             <ApolloWrapperWithSession>
